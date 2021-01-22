825bdbe270a044a35febe706ddb2e9fce7f8eb2e
AgnesMartinez <63485599+AgnesMartinez@users.noreply.github.com>
2021-01-21 18:40:54 -0600

    modificaciones para huachilate
    Vas a necesitar esas 2 tablas

--- crear_db.py
+++ crear_db.py
@@ -5,7 +5,6 @@ from tabulate import tabulate
 
 #Conexion a BD
 conn = sqlite3.connect('boveda.sqlite3')
-
 tabla_t = """CREATE TABLE IF NOT EXISTS transacciones (
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
     timestamp VARCHAR(255),
@@ -16,7 +15,25 @@ tabla_t = """CREATE TABLE IF NOT EXISTS transacciones (
 
 tabla_comentario = """CREATE TABLE IF NOT EXISTS comentarios (
     ID INTEGER PRIMARY KEY AUTOINCREMENT,
-    id_comment VARCHAR(255))"""
+    id_comment VARCHAR(255) UNIQUE)"""
+
+tabla_rifa = """CREATE TABLE IF NOT EXISTS huachilate (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp VARCHAR(255),
+    huachiclave VARCHAR(255),
+    cantidad INTEGER,
+    entregado INTEGER)"""
+
+tabla_boletitos = """CREATE TABLE IF NOT EXISTS boletitos (
+    ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    timestamp VARCHAR(255),
+    usuario VARCHAR(255),
+    huachiclave VARCHAR(255)
+)"""
+
+index4 = """CREATE INDEX IF NOT EXISTS ind_usuario ON boletitos (usuario)"""
+
+index3 = """CREATE INDEX IF NOT EXISTS ind_huachiclave ON huachilate (huachiclave)"""
 
 index2 = """CREATE INDEX IF NOT EXISTS ind_comentario ON comentarios (id_comment)"""
 
@@ -36,6 +53,15 @@ cursor.execute(tabla_comentario)
 
 cursor.execute(index2)
 
+cursor.execute(tabla_rifa)
+
+cursor.execute(index3)
+
+cursor.execute(tabla_boletitos)
+
+cursor.execute(index4)
+
 conn.commit()
+
 
 
