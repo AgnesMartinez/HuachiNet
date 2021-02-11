d578475b02c43adb3c2378dc244c2413a4fbafc4
AgnesMartinez <63485599+AgnesMartinez@users.noreply.github.com>
2021-02-10 19:52:31 -0600

    Robos 3.0
    3 metodos nuevos en el huachicore
    
    Reconstruccion de slots()
    
    Mas cosas

--- core.py
+++ core.py
@@ -1,4 +1,5 @@
 import time
+from datetime import datetime
 import sqlite3
 import operator
 import random
@@ -25,15 +26,16 @@ class HuachiNet():
         self.conn = sqlite3.connect("boveda.sqlite3")
         self.cursor = self.conn.cursor()
         self.id = usuario
+        self.perk, self.power, self.trait, self.weapon = self.Consultar_Perks()
         self.saldo_total = self.Consultar_Saldo()
-        self.historial = self.Historial_Cuenta("Global")
-        self.depositos = self.Historial_Cuenta("Deposito")
-        self.retiros = self.Historial_Cuenta("Retiro")
-        self.asaltos = self.Historial_Cuenta("Asalto")
-        self.huachitos = self.Historial_Cuenta("Huachito")
-        self.premios_huachito = self.Historial_Cuenta("Premio Huachito")
-        self.atracos = self.Historial_Cuenta("Atraco")
-        self.levantones = self.Historial_Cuenta("Levanton")
+        self.historial = self.Historial_Cuenta()
+        self.depositos = self.Historial_Cuenta(tipo_movimiento="Deposito")
+        self.retiros = self.Historial_Cuenta(tipo_movimiento="Retiro")
+        self.asaltos = self.Historial_Cuenta(tipo_movimiento="Asalto")
+        self.huachitos = self.Historial_Cuenta(tipo_movimiento="Huachito")
+        self.premios_huachito = self.Historial_Cuenta(tipo_movimiento="Premio Huachito")
+        self.atracos = self.Historial_Cuenta(tipo_movimiento="Atraco")
+        self.levantones = self.Historial_Cuenta(tipo_movimiento="Levanton")
 
 
     def Bono_Bienvenida(self,usuario):
@@ -61,13 +63,12 @@ class HuachiNet():
         query = """SELECT ID FROM transacciones WHERE usuario=? LIMIT 1"""
 
         try:
-            self.cursor.execute(query,(usuario,))
+            self.cursor
 
-            resultado = self.cursor.fetchone()
-
-            print(resultado)
+            resultado = self.cursor.execute(query,(usuario,)).fetchone()
 
             if resultado != None:
+
                 existe = True
 
         except Exception as e:
@@ -116,7 +117,7 @@ class HuachiNet():
 
             resultado = self.cursor.fetchone()
 
-            return resultado[0][0]
+            return resultado[0]
         
         except Exception as e:
             print(f'----\n{e}')  
@@ -129,7 +130,7 @@ class HuachiNet():
                 parametros = (self.id,)
 
             else:
-                query2 = """SELECT id,timestamp,cantidad,origen_destino FROM transacciones WHERE usuario=? AND nota=? ORDER BY id DESC"""
+                query = """SELECT id,timestamp,cantidad,origen_destino FROM transacciones WHERE usuario=? AND nota=? ORDER BY id DESC"""
                 parametros = (self.id,tipo_movimiento)
 
             self.cursor.execute(query,parametros)
@@ -140,6 +141,7 @@ class HuachiNet():
         
         except Exception as e:
             print(f'----\n{e}')
+            
     
     def Mujicanos(self):
         """Lista de usuarios activos"""
@@ -153,14 +155,13 @@ class HuachiNet():
 
         return list(filter(None,resultado))
 
-    def Ranking(self, limite = '25'):
+    def Ranking(self):
         """Forbes Mujico - Usuarios Abinerados"""
 
         #Obtener lista de usuarios
-        query = """SELECT usuario, SUM(cantidad) as cantidad FROM transacciones WHERE usuario in (SELECT usuario FROM transacciones WHERE nota='Bono Inicial') GROUP BY usuario ORDER BY cantidad DESC LIMIT ? """
+        query = """SELECT usuario, SUM(cantidad) as cantidad FROM transacciones WHERE usuario in (SELECT usuario FROM transacciones WHERE nota='Bono Inicial') GROUP BY usuario ORDER BY cantidad DESC"""
         
-
-        return self.cursor.execute(query,(limite,)).fetchall()
+        return self.cursor.execute(query).fetchall()
 
     def Huachiclave(self):
         """Regresa la huachiclave vigente o genera una nueva"""
@@ -187,3 +188,56 @@ class HuachiNet():
         
         else:
             return resultado[-1]
+
+    def Consultar_Perks(self):
+        """Consultar perk, power, trait y arma"""
+
+        query = """SELECT perk,power,trait,weapon FROM perks WHERE usuario = ?"""
+
+        resultado = self.cursor.execute(query,(self.id,)).fetchall()
+
+        #En caso de que el usuario no este en la tabla de perks, se añadira con stats basicos.
+        if resultado == []:
+
+            timestamp = time.time()
+            
+            query2 = """INSERT INTO perks (timestamp,usuario,perk,power,trait,weapon) VALUES (?,?,?,?,?,?)"""
+
+            self.cursor.execute(query2,(timestamp,self.id,"Normal",100,"Normal","Navaja"))
+
+            self.conn.commit()
+
+            return ("Normal",100,"Normal","Navaja")
+
+        else:
+
+            return resultado[-1]
+
+    def Update_Perks(self,clase,item):
+        """Agregar y modificar perk,trait y weapon de los mujicanos"""
+
+        query = f"""UPDATE perks SET {clase} = ? WHERE usuario = ?"""
+
+        query2 = """UPDATE perks SET power = 100 WHERE usuario = ?"""
+
+        self.cursor.execute(query,(item,self.id))
+
+        if clase == "perk":
+            self.cursor.execute(query2,(self.id,))
+
+        self.conn.commit()
+
+    def Consumir_Energia(self,cantidad):
+        """Los perks necesitan energia para funcionar"""
+
+        energia = self.power - cantidad
+
+        query = """UPDATE perks SET power = ? WHERE usuario = ?"""
+
+        self.cursor.execute(query,(energia,self.id))
+
+        self.conn.commit()
+
+
+                
+
